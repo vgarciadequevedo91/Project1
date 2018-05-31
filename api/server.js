aea4896f09094f989b4cea43c02d45a9c947e866
Lauren Hastings <lh@Laurens-MacBook-Pro.local>
2018-05-31 17:14:26 -0400

    Finished production-ready server script

--- api/server.js
+++ api/server.js
@@ -13,7 +13,7 @@ app.listen(8081, function() {
   console.log('App running on port 8081.')
 })
 
-// TODO: Debug database connection
+// Dev connection, comment out for production deployment
 var dbConnection = mysql.createConnection({
   host: 'aaup68o0mixgl.ca8q3xhabeql.us-west-2.rds.amazonaws.com',
   port: '3306',
@@ -22,14 +22,13 @@ var dbConnection = mysql.createConnection({
   database: 'group5db'
 })
 
-/*
+// Production connection, comment out for local testing
 var dbConnection = mysql.createConnection({
   host: process.env.RDS_HOSTNAME,
   user: process.env.RDS_USERNAME,
   password: process.env.RDS_PASSWORD,
   port: process.env.RDS_PORT
 })
-*/
 
 // TODO: Finish officially connecting to database
 dbConnection.connect(function(error) {
@@ -43,8 +42,15 @@ dbConnection.connect(function(error) {
 
 // Add a new contact
 app.post('/contacts/', function (req, res) {
-  var query = dbConnection.query('INSERT INTO Contacts SET ?', req.body, function (error, results, fields) {
-    console.log(error)
+  var query = dbConnection.query('INSERT INTO Contacts SET ?', req.query, function (error, results, fields) {
+    if (error) error = 1
+    res.send(JSON.stringify({'status': 200, 'error': error, 'response': results}))
+  })
+})
+
+// Get a list of all contacts for a user
+app.get('/contacts/', function (req, res) {
+  var query = dbConnection.query('SELECT * FROM Contacts WHERE userID = "' + req.query.userID + '"', function (error, results, fields) {
     if (error) error = 1
     res.send(JSON.stringify({'status': 200, 'error': error, 'response': results}))
   })
@@ -52,11 +58,8 @@ app.post('/contacts/', function (req, res) {
 
 // Delete Contacts entry
 app.delete('/contacts/', function (req, res) {
-  console.log('Delete an existing entry')
-  res.send('Delete')
-})
-
-app.get('/contacts/', function (req, res) {
-  console.log('Get')
-  res.send('Get')
+  var query = dbConnection.query('DELETE FROM Contacts WHERE firstName = "' + req.query.firstName + '" AND lastName = "' + req.query.lastName + '"', function (error, results, fields) {
+    if (error) error = 1
+    res.send(JSON.stringify({'status': 200, 'error': error, 'response': results}))
+  })
 })
